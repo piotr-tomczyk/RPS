a3d00ccf2bb9baa007ba3b19f348e1c811c27686
piotr-tomczyk <travis.blonded@gmail.com>
2022-02-02 20:19:59 +0100

    Add UI

--- script.js
+++ script.js
@@ -1,119 +1,120 @@
-function Start(){
-    var player_score = 0;
-    var computer_score = 0;
-    console.log("THIS IS ROCK, PAPER, SCISSORS GAME !!! \n");
-    console.log("YOU WILL BE PLAYING AGAINST THE COMPUTER !!! \n")
-    console.log("------------------------------ \n");
-    var choice = prompt("PRESS ANY KEY AND THEN ENTER TO START !!!");
-    UI(player_score, computer_score);
-}
 function DrawSign(){
-    var min = 1;
     var max = 3;
     return Math.floor(Math.random()*(max));
 }
-function Exit(player_score, computer_score){
-    console.log("FINAL SCORE !!! \n");
-    console.log("PLAYER SCORE: " + player_score + "\n");
-    console.log("COMPUTER SCORE: " + computer_score + "\n");
-    console.log("------------------------------ \n");
-    if (player_score > computer_score){
-        console.log("PLAYER WINS !!! \n")
-    }
-    else if (computer_score > player_score){
-        console.log("COMPUTER WINS !!! \n");
-    }
-    else if (computer_score == player_score){
-        console.log("DRAW !!! \n");
-    }
-    console.log("------------------------------ \n");
-    return;
+function Start(){
+    var player_score = 0;
+    var computer_score = 0;
+    var round_count = 5;
+    alert("THIS IS ROCK, PAPER, SCISSORS GAME !!! \n YOU WILL BE PLAYING AGAINST THE COMPUTER !!!");
+    UI(player_score, computer_score, round_count);
 }
-function UI(player_score, computer_score){
-    console.log("CURRENT SCORE \n");
-    console.log("PLAYER SCORE: " + player_score + "\n");
-    console.log("COMPUTER SCORE: " + computer_score + "\n");
-    console.log("------------------------------ \n");
-    console.log("TYPE q, THEN ENTER TO EXIT !!! \n TYPE ANY OTHER KEY, THEN ENTER TO CONTINUE !!! \n");
-    console.log("------------------------------ \n");
-    var choice = prompt();
-    if (choice == "q"){
+function UI(player_score, computer_score, round_count){
+    const score = document.querySelector('#score');
+    const round_info = document.querySelector('#round-info');
+    if (round_count == 0){
+        score.textContent = player_score + " vs. " + computer_score;
         Exit(player_score, computer_score);
         return;
     }
     else{
-        Round(player_score, computer_score);
+        round_info.textContent = "ROUND " + (6 - round_count) + "/5";
+        score.textContent = player_score + " vs. " + computer_score;
+        Round(player_score, computer_score, round_count);
     }
 }
-function Round(player_score, computer_score){
-    var RPS = ['ROCK', 'PAPER', 'SCISSORS'];
-    var valid = true;
-    while(valid){
-        console.log("PRESS 1 FOR STONE \n");
-        console.log("PRESS 2 FOR PAPER \n");
-        console.log("PRESS 3 FOR SCISSORS \n");
-        var player_pick = prompt();
-        if (player_pick == "1" || player_pick == "2" || player_pick == "3"){
-            valid = false;
-        }
-        else{
-            valid = true;
-        }
+function Exit(player_score, computer_score){
+    if (player_score > computer_score){
+        alert("FINAL SCORE !!! \n PLAYER SCORE: " + player_score + "\n COMPUTER SCORE: " + computer_score + "\n PLAYER WINS !!!");
     }
-    var player_pick = parseInt(player_pick) - 1;
+    else if (computer_score > player_score){
+        alert("FINAL SCORE !!! \n PLAYER SCORE: " + player_score + "\n COMPUTER SCORE: " + computer_score + "\n COMPUTER WINS !!!");
+    }
+    else if (computer_score == player_score){
+        alert("FINAL SCORE !!! \n PLAYER SCORE: " + player_score + "\n COMPUTER SCORE: " + computer_score + "\n DRAW !!!");
+    }
+    Start();
+    return;
+}
+function Round(player_score, computer_score, round_count){
+    var player_pick = 0;
+    var lock = false;
+    var rock = document.querySelector("#rock");
+    var paper = document.querySelector("#paper");
+    var scissors = document.querySelector("#scissors");
+    rock.addEventListener('click', function (){
+        if(!lock){
+            player_pick = 0;
+            lock = true;
+            ContinueRound(player_score, computer_score, round_count, player_pick);
+        }
+    });
+    paper.addEventListener('click', function (){
+        if(!lock){
+            player_pick = 1;
+            lock = true;
+            ContinueRound(player_score, computer_score, round_count, player_pick);
+        }
+    });
+    scissors.addEventListener('click', function (){
+        if(!lock){
+            player_pick = 2;
+            lock = true;
+            ContinueRound(player_score, computer_score, round_count, player_pick);
+        }
+    });
+}
+function ContinueRound(player_score, computer_score, round_count, player_pick){
+    var RPS = ['ROCK', 'PAPER', 'SCISSORS'];
     var computer_pick = DrawSign();
-    console.log(RPS[player_pick] + " VS. " + RPS[computer_pick] + "\n");
-    console.log("------------------------------ \n");
+    alert(RPS[player_pick] + " VS. " + RPS[computer_pick]);
     if (player_pick == 0){
         if(computer_pick == 0){
-            console.log("DRAW !!! \n")
-            console.log("EVERYBODY GETS A POINT !!! \n")
+            alert("DRAW !!! \n EVERYBODY GETS A POINT !!!");
             computer_score ++;
             player_score ++;
         }
         if(computer_pick == 1){
-            console.log("COMPUTER WINS !!! \n")
+            alert("COMPUTER WINS !!! \n");
             computer_score ++;
         }
         if(computer_pick == 2){
-            console.log("PLAYER WINS !!! \n")
+            alert("PLAYER WINS !!! \n");
             player_score ++;
         }
     }
     if (player_pick == 1){
         if(computer_pick == 0){
-            console.log("PLAYER WINS !!! \n")
+            alert("PLAYER WINS !!! \n");
             player_score ++;
         }
         if(computer_pick == 1){
 
-            console.log("DRAW !!! \n")
-            console.log("EVERYBODY GETS A POINT !!! \n")
+            alert("DRAW !!! \n EVERYBODY GETS A POINT !!!")
             computer_score ++;
             player_score ++;
         }
         if(computer_pick == 2){
-            console.log("COMPUTER WINS !!! \n")
+            alert("COMPUTER WINS !!! \n")
             computer_score ++;
         }
     }
     if (player_pick == 2){
         if(computer_pick == 0){
-            console.log("COMPUTER WINS !!! \n")
+            alert("COMPUTER WINS !!! \n")
             computer_score ++;
         }
         if(computer_pick == 1){
-            console.log("PLAYER WINS !!! \n")
+            alert("PLAYER WINS !!! \n")
             player_score ++;
         }
         if(computer_pick == 2){
-            console.log("DRAW !!! \n")
-            console.log("EVERYBODY GETS A POINT !!! \n")
+            alert("DRAW !!! \n EVERYBODY GETS A POINT !!!")
             computer_score ++;
             player_score ++;
         }
     }
-    console.log("------------------------------ \n")
-    UI(player_score, computer_score);
+    round_count--;
+    UI(player_score, computer_score, round_count);
 }
 Start();
